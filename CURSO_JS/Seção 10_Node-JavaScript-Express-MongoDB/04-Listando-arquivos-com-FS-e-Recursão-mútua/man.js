8c9259d6ddc94ba054a658f79170d7474e216c32
Fernando Pereira Xavier <fernandoxavier233@gmail.com>
2022-12-03 22:37:18 -0300

    atualizações

--- CURSO_JS/Seção 10_Node-JavaScript-Express-MongoDB/04-Listando-arquivos-com-FS-e-Recursão-mútua/man.js
+++ CURSO_JS/Seção 10_Node-JavaScript-Express-MongoDB/04-Listando-arquivos-com-FS-e-Recursão-mútua/man.js
@@ -1,23 +1,26 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
 // console.log(__dirname);
 
 async function readdir(routDir) {
   routDir = routDir || path.resolve(__dirname);
-  const files = await fs.readdirSync(routDir);
+  const files = await fs.readdir(routDir);
   recebeArquivos(files, routDir);
 }
 
 async function recebeArquivos(files, routDir) {
   for (const i of files) {
     const leerTodosOsArqivos = path.resolve(routDir, i);
-    const stats = await fs.statSync(leerTodosOsArqivos);
+    const stats = await fs.stat(leerTodosOsArqivos);
+    if (/\.git/g.test(leerTodosOsArqivos)) continue;
+    if(/node_modules/.test(leerTodosOsArqivos)) continue;
     if (stats.isDirectory()) {
       readdir(leerTodosOsArqivos);
       continue;
     }
     
-    console.log(i, + "-----------------------");
+    if (!/\.js$/g.test(leerTodosOsArqivos))continue
+    console.log(i);
   }
 }
 
