68d57c36ed582872e0878ae160413d143579c7bf
fernando <fernandoxavier233@gmail.com>
2022-12-15 21:30:17 -0300

    express e webpack

---
+++ curso-js/Seção 10_Node-JavaScript-Express-MongoDB/05-Express/7-express-webpack/frontend/index.js
@@ -0,0 +1,6 @@
+import "regenerator-runtime"
+import "core-js/stable"
+// import form from "./modules/form"
+import './assets/css/style.css'
+// form()
+
---
+++ curso-js/Seção 10_Node-JavaScript-Express-MongoDB/05-Express/7-express-webpack/server.js
@@ -0,0 +1,18 @@
+/*
+
+*/
+
+const express = require("express");
+const app = express();
+const path = require("path");
+const routes = require("./routes");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, 'public')));
+app.set("views", path.resolve(__dirname, "src", "views"))
+app.set("view engine", "ejs")
+app.use(routes);
+
+app.listen(3000, () => {
+  console.log("Servidor rodando na porta 3000.\n Acesse http://localhost:3000")
+})
